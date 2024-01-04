debf33f3cabde6b37ec59fd2ed9db6be72376151
Nagarjun <111555377+nagarjun73@users.noreply.github.com>
2024-01-04 23:48:31 +0530

    added login fuctionality and generating token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,16 @@ const { checkSchema } = require('express-validator')
 
 const userCltr = require('../app/controllers/userCltr')
 
-const { userRegValidationSchema } = require('../app/helpers/userValidation')
+const {
+  userRegValidationSchema,
+  userLoginValidationSchema } = require('../app/helpers/userValidation')
 
 const router = express.Router()
 
 //Register User
-router.get('/register', checkSchema(userRegValidationSchema), userCltr.register)
+router.post('/register', checkSchema(userRegValidationSchema), userCltr.register)
+
+//Login User
+router.post('/login', checkSchema(userLoginValidationSchema), userCltr.login)
 
 module.exports = router

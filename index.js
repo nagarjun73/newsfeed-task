4d6fd06e3ee58bb63c55d92519b03a68f3f1604b
Nagarjun <111555377+nagarjun73@users.noreply.github.com>
2024-01-04 20:50:41 +0530

    setting up routes, model, validation

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require("cors")
+const mongoose = require('mongoose')
 const port = process.env.PORT
 
+//Route imports
+const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 
@@ -11,8 +14,12 @@ app.use(express.json())
 //cors enabled
 app.use(cors())
 
+//user routes
+app.use("/user", userRoutes)
 
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 })
+
+module.exports = app

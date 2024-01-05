861201ad6208b1b6f37ae7a4ad0cf00311eaa441
Nagarjun <111555377+nagarjun73@users.noreply.github.com>
2024-01-05 09:37:41 +0530

    used xml2js to parse XML

--- app/controllers/feedCltr.js
+++ app/controllers/feedCltr.js
@@ -1,13 +1,23 @@
 const axios = require('axios')
-
+const { parseString } = require('xml2js')
 const feedCltr = {}
 
 feedCltr.recentfeeds = async (req, res) => {
   try {
     const getRecentFeeds = await axios.get('https://timesofindia.indiatimes.com/rssfeedmostrecent.cms')
-    res.json(getRecentFeeds.data);
+    const resultInXml = getRecentFeeds.data
+
+    parseString(resultInXml, (error, result) => {
+      if (error) {
+        res.status(400).json(error)
+      } else {
+        const parsedFeed = result.rss.channel[0].item
+        res.json(parsedFeed)
+      }
+    })
+
   } catch (e) {
-    res.json(e)
+    res.status(400).json(e)
   }
 }
 
